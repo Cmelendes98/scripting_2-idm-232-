237d66540f8e35e8ec4a85bb70d3d997cb49aaf9
Craigmelendes <cm3577@drexel.edu>
2018-04-27 10:54:42 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+ 	<head> 
+ 		 <link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<div id="mySidenav" class="sidenav">
+		  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+		  <a href='resume.html'> Resume </a>
+		  <a href='contact.html'> Contact </a>
+		  <a href='digital-design.html'> Digital Design </a>
+		  <a href='fine-art.html'> Fine Art </a>
+
+		</div>
+
+	<div id="main">
+		<header>
+			<span class= "openLink" id= 'openLink' onclick="openNav()"><img src="hamburger-icon.png"/> </span>
+			<div class='logo'> 
+				<a href='index.php'>
+					<img src= "imgs/logo.png"/> 
+				</a>
+			</div>
+			<nav class="clearfix">
+				<div class='noborder'>
+					<a href='index.php'> Home </a>
+				</div>
+				<div>
+					 <a href=''>Recipies</a>
+				</div>
+				<div>
+					 <a href='' class='search-link'>Search <img class='arrow' src='imgs/arrow.png'/></a>
+				</div>
+			</nav>
+		
+		</header>
+		<div class='body_wrap home_page'> 
+			
+		</div>
+		<footer>
+			
+		</footer>
+		<script type='text/javascript' src='mobileNav.js'></script>
+	</div>
+	</body>
+</html>
