82dadcb7173536ab0a3d93e97603b32697ac979d
Craigmelendes <cm3577@drexel.edu>
2018-04-27 12:43:21 -0400

    alpha homepage
    - placeholder video
        - add footer
        - placeholder icons
        - placeholder body text

--- index.php
+++ index.php
@@ -36,8 +36,41 @@
 		</header>
 		<div class='body_wrap home_page'> 
 			
+			<div class="fullscreen-bg">
+    			<video loop muted autoplay class="fullscreen-bg__video">
+        			<source src="imgs/cms.mp4" type="video/mp4">
+   		 		</video>
+			</div>
+			<div class = "flex-container">
+				<div class = "flex-item" >
+					<img src= "imgs/logo.png"/> 
+				</div>
+				<div class = "flex-item" >
+					<img src= "imgs/logo.png"/> 
+				</div>
+				<div class = "flex-item" >
+					<img src= "imgs/logo.png"/> 
+				</div>
+
+			</div>
+			<div class ="flex-container" >
+				<div class ="flex-item" >
+					<h1> Our Mission </h1>
+					<p> 
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</div>
+			</div>
+
 		</div>
 		<footer>
+			<a href='index.php'> Home </a>
+			<span class = "divider">|</span>
+			<a href=''>Recipies</a>
+			<span class = "divider">|</span> 
+			<a href=''>Search</a>
+
+
 			
 		</footer>
 		<script type='text/javascript' src='mobileNav.js'></script>
